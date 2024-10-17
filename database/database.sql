5d76adbb80c054652bf4fee261cbe86fcee14ab7
Ali-TBB <Ali-Tbb@git.com>
2024-10-17 12:13:37 +0100

    feat: Add difficulty field to quizzes and questions tables
    The code changes include adding a `difficulty` field to the `quizzes` and `questions` tables in the database schema. The `difficulty` field is a VARCHAR type with a length of 10 and has a check constraint to only allow values of 'easy', 'medium', 'hard', or 'mixed'. This addition will allow for categorizing quizzes and questions based on their difficulty level.

--- database/database.sql
+++ database/database.sql
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS `dataset_items` (
 CREATE TABLE IF NOT EXISTS `quizzes` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `topic` VARCHAR(255) ,
+  `difficulty` VARCHAR(10) NOT NULL CHECK (`difficulty` IN ('easy', 'medium', 'hard', 'mixed')),
   `number_of_questions` INT NOT NULL,
   `created_at` TIMESTAMP,
   `updated_at` TIMESTAMP
@@ -37,6 +38,8 @@ CREATE TABLE IF NOT EXISTS `attachments` (
 CREATE TABLE IF NOT EXISTS `questions` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `quiz_id` INT NOT NULL,
+  `question_type` VARCHAR(255) NOT NULL CHECK (`question_type` IN ('multiple-answer', 'one-answer')),
+  `difficulty` VARCHAR(10) NOT NULL CHECK (`difficulty` IN ('easy', 'medium', 'hard')),
   `content` TEXT NOT NULL,
   `options_ids` TEXT,
   `correct_option_id` INT,
@@ -53,6 +56,7 @@ CREATE TABLE IF NOT EXISTS `options` (
   `updated_at` TIMESTAMP,
   FOREIGN KEY (`question_id`) REFERENCES questions(`id`) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS `answers` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `question_id` INT NOT NULL,
